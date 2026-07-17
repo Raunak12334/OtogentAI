c19ee25695904d2c558343141ac86c42486a0ede
raunak <raunaksahu61@gmail.com>
2026-07-17 15:23:58 +0530

    7] feat: implement Inngest background job system with multi-provider AI text generation support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,38 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
-import { requireAuth } from "@/lib/auth-utils";
 import { useTRPC } from "@/trpc/client";
-import { caller } from "@/trpc/server";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 const Page = () => {
   const trpc = useTRPC();
   const queryClient = useQueryClient();
   const { data } = useQuery(trpc.getUsers.queryOptions());
+  const testAI = useMutation(trpc.testAI.mutationOptions({
+    onSuccess: () => {
+      toast.success("AI job Queued")
+    }
+  }
+  ));
   const create = useMutation(trpc.createUser.mutationOptions({
     onSuccess: () => {
       queryClient.invalidateQueries(trpc.getUsers.queryOptions());
     },
+
 
   }));
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       <div>{JSON.stringify(data, null, 2)}</div>
+      <Button disabled={testAI.isPending} onClick={() => testAI.mutate()} >
+        TestAI
+      </Button>
       <Button disabled={create.isPending} onClick={() => create.mutate()}>
         Create Workflow
       </Button>
+
       <Button>
         Logout
       </Button>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,15 @@
 import prisma from '@/lib/db';
 import { createTRPCRouter, protectedProcedure } from '../init';
 import { inngest } from '@/inngest/client';
+import { google } from '@ai-sdk/google';
+import { generateText } from 'ai';
 
 export const appRouter = createTRPCRouter({
+    testAI: protectedProcedure.mutation(async () => {
+        await inngest.send({ name: "process/ai" })
+        return { success: true, message: "job queued" }
+    }),
+
     getUsers: protectedProcedure.query(({ ctx }) => {
         return prisma.user.findMany();
     }),
